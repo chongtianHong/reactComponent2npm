cbb1c8aa01dce40822ff571f4861b39f84a6d829
chongtianHong <450707109@qq.com>
2019-09-02 12:03:58 +0800

    Create index.js
    组件源代码

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as styles from './index.css';
+class ReactDemo extends React.Component{
+  render () {
+    return <div className={styles.wrapper}>hello world</div>
+  }
+}
+export default ReactDemo;

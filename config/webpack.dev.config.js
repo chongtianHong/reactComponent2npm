973a78c69319547d56b55179ead4c4c58eed1ff9
chongtianHong <450707109@qq.com>
2020-03-21 14:53:34 +0800

    Update webpack.dev.config.js

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -25,6 +25,7 @@ const devConfig = {
 	devServer: {
     contentBase: path.join(__dirname, '../example/src/'),
     compress: true,
+    host: '127.0.0.1', // webpack-dev-server启动时要指定ip，不能直接通过localhost启动，不指定会报错
     port: 3001, // 启动端口为 3001 的服务
     open: true // 自动打开浏览器
   },
